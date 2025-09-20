3305d0e0b628be6e7ee9f9e0a181a0df5afe8ba4
playercool <playercool02@gmail.com>
2025-09-20 12:16:09 +0530

    first commit

---
+++ mma_backend/cmd/mma_api/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"mma_api/internal/config"
+	"mma_api/internal/utils/response"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	//config
+	cfg := *config.Must_Load()
+
+	//setup routers
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /api/man", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Hello, this is a test response from /api/man")
+		response.WriteJson(w, 200, "yoo")
+	})
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Http_Server.Addr,
+		Handler: router,
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("server is not starting")
+		}
+	}()
+	<-done
+	slog.Info("server is shutting down")
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Info("server not closing ")
+	}
+
+}
